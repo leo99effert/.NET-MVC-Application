730af2834f97261c35bdef92d28f38aa0a6600c1
leo99effert <leo99effert@outlook.com>
2022-03-31 08:58:54 +0200

    Add project files.

---
+++ NextHome/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NextHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
