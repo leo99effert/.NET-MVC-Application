00b841a6005703684355e094ec2c241401f046a1
leo99effert <leo99effert@outlook.com>
2022-04-04 18:12:29 +0200

    working on roles

--- NextHome/Controllers/AccountController.cs
+++ NextHome/Controllers/AccountController.cs
@@ -7,10 +7,12 @@ namespace NextHome.Controllers
     public class AccountController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AccountController(RoleManager<IdentityRole> roleManager)
+        public AccountController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [HttpGet]
@@ -48,6 +50,33 @@ namespace NextHome.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} was not found";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+            };
+
+            foreach (var user in userManager.Users)
+            {
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
         }
     }
 }
